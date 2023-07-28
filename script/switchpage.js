d8d21bfd21929f89ee5972373143593025a89177
Nyx <nv.schutz@gmail.com>
2023-07-28 12:19:19 +0200

    page switching implemented

---
+++ script/switchpage.js
@@ -0,0 +1,18 @@
+var containers = document.getElementsByClassName("pageContainers");
+var activepagename = document.getElementById("activepagename");
+const links = document.querySelectorAll('.navlink');
+
+function switchPage(event) {
+  const thisLink = event.target;
+  var currentID = thisLink.id;
+  links.forEach(link => {
+    link.classList.remove('activepage');
+  });
+  thisLink.classList.add("activepage");
+  activepagename.innerHTML = thisLink.innerHTML;
+  document.body.style.backgroundColor = "var(--" + currentID + ")";
+  for (var i = 0; i < containers.length; i++) {
+    containers[i].classList.add('hide');
+  }
+  document.getElementById(currentID + "Container").classList.remove('hide');
+}
